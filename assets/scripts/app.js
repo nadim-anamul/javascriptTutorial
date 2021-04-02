2471f4824ac032688112d99a63ae03658f1e98c0
nadim-anamul <nadim.csm@gmail.com>
2021-04-02 22:24:35 +0600

    Variable declearation and initial project setup

---
+++ assets/scripts/app.js
@@ -0,0 +1,5 @@
+const defaultResult = 0;
+let currentResult = 0;
+currentResult = ( currentResult + 10 ) * 3 / 2;
+let calculationDescription = '( ' + defaultResult + ' + 10 ) * 3 / 2';
+outputResult( currentResult , calculationDescription );
