f7441f40a3297d15b3178ff8146e46543d8806ff
nadim-anamul <nadim.csm@gmail.com>
2021-04-05 23:26:54 +0600

    function lecture

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,8 @@
 const defaultResult = 0;
-let currentResult = 0;
-currentResult = ( currentResult + 10 ) * 3 / 2;
-let calculationDescription = '( ' + defaultResult + ' + 10 ) * 3 / 2';
-outputResult( currentResult , calculationDescription );
+let currentResult = defaultResult;
+function add() {
+    currentResult = currentResult + userInput.value;
+    outputResult( currentResult , '' );
+}
+
+addBtn.addEventListener('click', add);
