8636e70977801be767692fc601c09c3cc75b75ab
nadim-anamul <nadim.csm@gmail.com>
2021-04-07 13:38:20 +0600

    task1 - Count dom element in a html file

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,16 @@
-const defaultResult = 0;
-let currentResult = 0;
-currentResult = ( currentResult + 10 ) * 3 / 2;
-let calculationDescription = '( ' + defaultResult + ' + 10 ) * 3 / 2';
-outputResult( currentResult , calculationDescription );
+// const defaultResult = 0;
+// let currentResult = 0;
+// currentResult = ( currentResult + 10 ) * 3 / 2;
+// let calculationDescription = '( ' + defaultResult + ' + 10 ) * 3 / 2';
+// outputResult( currentResult , calculationDescription );
+
+/**
+ * Count the number of element
+ * return the number
+ */
+
+function countHtmlElement(dom) {
+    return document.getElementsByTagName(''+dom).length
+}
+let result = countHtmlElement('button'); // call countHtmlElement function with the parameter and keep the return data in result variable
+console.log(result); // print the result data in console
