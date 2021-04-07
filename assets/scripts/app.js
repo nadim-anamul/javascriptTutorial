1b0c096a150e9c7f462ea0a8854249d31872f40e
nadim-anamul <nadim.csm@gmail.com>
2021-04-07 16:40:57 +0600

    task3 - Create a html element

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,16 @@
-const defaultResult = 0;
-let currentResult = 0;
-currentResult = ( currentResult + 10 ) * 3 / 2;
-let calculationDescription = '( ' + defaultResult + ' + 10 ) * 3 / 2';
-outputResult( currentResult , calculationDescription );
+/**
+ * Create an function which is accept two parameter 
+ * first is element name like p, h1, div etc and 
+ * second parameter is rest of the parameter or an object of attributes of that element. 
+ * finally return the html node.
+ */
+ function createHtmlElement(nodeName, nodeValues) {
+     let values = '';
+     for (const value in nodeValues) {
+        values += '' + value + '="'+ nodeValues[value] + '" ';
+     }
+     let dom = `<${nodeName} ${values}></${nodeName}>`;
+     return dom;
+ }
+ console.log( createHtmlElement( 'p', {id : "test", name : "test", style : "padding:0" } ) );
+ //createHtmlElement('p', {id : "test", name : "test", style : "padding:0" });
