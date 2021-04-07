6fc387fce611897201f9022c575c2a998a71a721
nadim-anamul <nadim.csm@gmail.com>
2021-04-07 22:59:03 +0600

    task3 - Stop all activity on the html page when webpage is loading.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,1 @@
-const defaultResult = 0;
-let currentResult = 0;
-currentResult = ( currentResult + 10 ) * 3 / 2;
-let calculationDescription = '( ' + defaultResult + ' + 10 ) * 3 / 2';
-outputResult( currentResult , calculationDescription );
+window.onload = function() { document.getElementById("hideAll").style.display = "none"; }
